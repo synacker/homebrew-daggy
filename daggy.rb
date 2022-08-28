da4e4460a3d7c66684d98895e5c9e6cf82caa488
synacker <synacker@yandex.ru>
2022-08-28 19:26:08 +0300

    Update to 2.1.3

--- daggy.rb
+++ daggy.rb
@@ -1,8 +1,8 @@
 class Daggy < Formula
   desc "Daggy - Data Aggregation Utility and C/C++ developer library for data streams catching"
   homepage "https://daggy.dev"
-  url "https://github.com/synacker/daggy/archive/refs/tags/2.1.2.zip"
-  sha256 "115249d58a4282ce112fa9efda7bd4b5630783440abb45f28b671a4fee017055"
+  url "https://github.com/synacker/daggy/archive/refs/tags/v2.1.3.zip"
+  sha256 "20dd0abe711e5435c3818026b4356392341cf91d217c37a0d7ee2ab7d112763e"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -11,22 +11,17 @@ class Daggy < Formula
   depends_on "yaml-cpp"
   depends_on "libssh2"
 
-  patch do
-    url "https://raw.githubusercontent.com/synacker/homebrew-daggy/master/mustache.patch"
-    sha256 "58ca775e378ea16cd0900be5fc325cf5bcbb2ab46d35d03af6c7f2f3a9f54b04"
-  end
-
   def install
-    system "wget", "https://raw.githubusercontent.com/kainjow/Mustache/master/mustache.hpp", "-O", "src/Daggy/mustache.hpp"
+    system "wget", "https://raw.githubusercontent.com/kainjow/Mustache/master/mustache.hpp", "-O", "src/mustache.hpp"
     system "mkdir", "build"
     
     Dir.chdir('build')
-    system "cmake", "-D", "VERSION=2.1.2", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "-D", "BUILD_SHARED_LIBS=ON", "../src"
+    system "cmake", "-D", "VERSION=2.1.3", "-D", "CMAKE_INSTALL_PREFIX:PATH=#{prefix}", "-D", "CMAKE_BUILD_TYPE=Release", "-D", "BUILD_SHARED_LIBS=ON", "../src"
     system "make", "all"
     system "make", "install"
   end
 
   test do
-    assert_match "daggy 2.1.2", shell_output("#{bin}/daggy -v")
+    assert_match "daggy 2.1.3", shell_output("#{bin}/daggy -v")
   end
 end
