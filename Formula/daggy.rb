91a53964bd686e8ff433adcec95ebea79fe43f42
synacker <synacker@yandex.ru>
2025-06-07 22:54:23 +0300

    Update to v2.2.1

--- Formula/daggy.rb
+++ Formula/daggy.rb
@@ -1,8 +1,8 @@
 class Daggy < Formula
   desc "Daggy - Data Aggregation Utility"
   homepage "https://daggy.gitbook.io"
-  url "https://github.com/synacker/daggy/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "5f813c1619167d3bd981c811ee0afca4b59140a659b41602737fdfb7b66e182e"
+  url "https://github.com/synacker/daggy/archive/refs/tags/v2.2.1.tar.gz"
+  sha256 "15f9c1902c5a2c20717e1de0b48bd32f86b1c768b9b09378483b1260ea957432"
   license "MIT"
 
   depends_on "cmake" => :build
